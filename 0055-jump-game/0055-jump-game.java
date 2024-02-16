c684e190170ad014e056637888dc802b5040fcef
Vijay Verma <121660419+Vijayveema@users.noreply.github.com>
2024-02-17 01:31:10 +0530

    Time: 2 ms (84.37%), Space: 45.7 MB (35.69%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int destination= 0;
+        for(int i=0;i<nums.length &&i<=destination;i++){
+            destination=Math.max(destination,i+nums[i]);
+            if(destination>=nums.length-1)
+                return true;
+        }
+        return false;
+        
+    }
+}
